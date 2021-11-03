447d5556bdef9dec52b8dbda5a6e906b702ee1c6
MaxiTrien <51158324+MaxiTrien@users.noreply.github.com>
2021-11-03 13:37:22 +0100

    Export to csv

--- export_to_anki.py
+++ export_to_anki.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import os
 
-def export_to_ankiformat(data):
-    pass
+
+def data_to_csv(data, result_dir):
+    df = pd.DataFrame(data) 
+    df.to_csv(os.path.join(result_dir, 'textmarker.csv'), encoding='utf-8', 
+              index=False, header=False)
+    print('CSV file created.')
